b1858ec42b69d3204138a093c6e684975090a8e9
machenmusik <machenmusik@gmail.com>
2016-11-24 03:28:24 -0500

    limit controller list rebuilds to roughly 1 second intervals

--- src/systems/tracked-controls.js
+++ src/systems/tracked-controls.js
@@ -8,6 +8,7 @@ module.exports.System = registerSystem('tracked-controls', {
   init: function () {
     var self = this;
     this.controllers = [];
+    this.lastControllerCheck = 0;
     if (!navigator.getVRDisplays) { return; }
     navigator.getVRDisplays().then(function (displays) {
       if (displays.length > 0) {
@@ -17,14 +18,19 @@ module.exports.System = registerSystem('tracked-controls', {
   },
 
   tick: function () {
-    var gamepads = navigator.getGamepads && navigator.getGamepads();
-    var gamepad;
-    var controllers = this.controllers = [];
-    var i;
-    if (!gamepads) { return; }
-    for (i = 0; i < gamepads.length; ++i) {
-      gamepad = gamepads[i];
-      if (gamepad && gamepad.pose) { controllers.push(gamepad); }
+    var now = Date.now();
+    if (now >= this.lastControllerCheck + 1000) {
+      this.lastControllerCheck = now;
+      // console.log('systems/tracked-controls tick lastControllerCheck ' + this.lastControllerCheck);
+      var gamepads = navigator.getGamepads && navigator.getGamepads();
+      var gamepad;
+      var controllers = this.controllers = [];
+      var i;
+      if (!gamepads) { return; }
+      for (i = 0; i < gamepads.length; ++i) {
+        gamepad = gamepads[i];
+        if (gamepad && gamepad.pose) { controllers.push(gamepad); }
+      }
     }
   }
 });
